593d3dc9d86173b5c621c005c220299fdf3ceb8d
dmytrokamina <dmitriy.kamina@gmail.com>
2026-05-12 15:34:27 +0200

    e

--- test.py
+++ test.py
@@ -1,25 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 API_key = os.getenv('api_key')
 
 
 import requests
-from datetime import datetime , timedelta
 
-
-def get_hist_data(lat,lon,start):
-    url_coord = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={'part'}&appid={'api_key'}'
-    res = requests.get(url_coord)
+def get_weather(lat, lon):
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=metric'
+    res = requests.get(url)
     data = res.json()
     return data
-    # temp = []
-    # for hour in data["hourly"]:
-    #     t = hour["temp"]
-    #     temp.append(t)     
-    # return data , temp
 
-
-get_hist_data(11.371151787523646,44.508646872523734,datetime.now())
+# Bologna: lat=44.518009, lon=11.361852
+get_weather(44.518009, 11.361852)
